976fda5ff7d044061fcc70363f50a3ee68b5a82e
Deborah Boateng <debbyyaaboat1992@gmail.com>
2023-12-04 23:11:45 +0100

    first innitial

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,24 @@
+import { Fragment } from "react"
+import { Link, Outlet} from "react-router-dom"
+import './navigation.styles.scss'
+/*import {ReactComponent as CrwnLogo} from  "../../assets/crown.svg"*/
+
+const Navigation = () => {
+    return(
+        <Fragment>
+            <div className="navigation">
+                <Link className="logo-container" to={'/'}>
+                <div className='logo'>Logo</div>
+                </Link>
+                <div className="nav-links-container">
+                 <Link className="nav-link" to={'/shop'}>SHOP</Link>
+                 <Link className="nav-link" to={'/auth'}>SIGN-IN</Link>
+
+                </div>
+            </div>
+            <Outlet/> 
+        </Fragment>
+    )
+  }
+  export default Navigation
+  
---
+++ src/utils/firebase/firebase.utils.jsx
@@ -0,0 +1,88 @@
+
+
+import {initializeApp} from 'firebase/app';
+import {
+    getAuth,
+    signInWithRedirect,
+    signInWithPopup,
+    GoogleAuthProvider,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+} from 'firebase/auth';
+
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc
+} from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDW5YbtA0whu9_2I0H0YjtExkmRb_ReoyA",
+    authDomain: "crown-clothing-db-b2493.firebaseapp.com",
+    projectId: "crown-clothing-db-b2493",
+    storageBucket: "crown-clothing-db-b2493.appspot.com",
+    messagingSenderId: "840445604461",
+    appId: "1:840445604461:web:bdb9af03429228f72bc564"
+  };
+  
+  // Initialize Firebase
+  const firebaseApp = initializeApp(firebaseConfig);
+  const googleProvider = new GoogleAuthProvider();
+  googleProvider.setCustomParameters({
+    prompt: 'select_account',
+  })
+
+  export const auth = getAuth();
+  export const signInWithGooglePopup = () => signInWithPopup(auth,googleProvider);
+  export const signInWithGoogleRedirect = () => signInWithGoogleRedirect(auth,googleProvider);
+
+
+  export const db = getFirestore();
+
+  export const createUserDocumentFromAuth = async (userAuth,
+    additionalInformation ={}) => {
+    if(!userAuth) return;
+
+    const userDocRef = doc(db,'users',userAuth.uid)
+
+    const userSnapshot = await getDoc(userDocRef)
+    
+
+if(!userSnapshot.exists()){
+    const {displayName, email} = userAuth;
+    const createdAt = new Date();
+    try{
+        await setDoc(userDocRef, {
+            displayName,email,createdAt,...additionalInformation,
+        });
+    }catch(error){
+        console.log('error creating a user',error.message)
+    }
+}
+
+return userDocRef;
+  };
+
+export const createAuthUserWithEmailAndPassword = async (email,password) => {
+    if(!email || !password) return;
+    return await createAuthUserWithEmailAndPassword(auth, email, password);
+};
+
+export const signInAuthUserWithEmailAndPassword = async (email,password) => {
+    if(!email || !password) return;
+    return await signInAuthUserWithEmailAndPassword(auth, email, password);
+};
+
+
+
+
+
+
+
+
+
+
+
+  
+
